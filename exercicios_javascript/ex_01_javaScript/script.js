ec32e2a5c700908a3b7960fc9781be7fc185e0e9
Niedja92 <niedleticia@hotmail.com>
2023-11-30 15:43:44 -0300

    first-commit-ex-01-js

---
+++ exercicios_javascript/ex_01_javaScript/script.js
@@ -0,0 +1,13 @@
+//  Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma. Antes de começar a codificação, identifiquem os tipos de cada variável. Em seguida, desenvolvam o código utilizando exemplos para cada variável que respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos tipos de dados corretos.
+
+let nome = "Letícia";
+let salario = 2000;
+let idade = 31;
+let diploma = true;
+
+document.write("Nome: " + nome + "<br/>");
+document.write("Salário: " + salario + "<br/>");
+document.write("Idade: " + idade + "<br/>");
+document.write("Possui diploma: " + diploma);
+
+
