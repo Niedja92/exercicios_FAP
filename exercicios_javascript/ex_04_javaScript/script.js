96e96c8a658fb6777f254085f53492d420302949
Niedja92 <niedleticia@hotmail.com>
2023-11-30 20:24:42 -0300

    correcting-word

--- exercicios_javascript/ex_04_javaScript/script.js
+++ exercicios_javascript/ex_04_javaScript/script.js
@@ -1,4 +1,4 @@
-// Crie um programa em JavaScript que contenha os seguintes tipos de funções: 
+// Crie um programa que contenha os seguintes tipos de funções: 
 // 1. uma função tradicional com a palavra reservada "Function" e sem nenhum parâmetro; 
 // 2. uma função tradicional com parâmetros e um retorno de valor; 
 // 3. uma arrow function com parâmetros e que retorne um valor. 
